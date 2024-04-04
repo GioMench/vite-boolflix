52250935af1c3d2246373ba557613ac71d1743bb
GioMench <giorgiaglm@gmail.com>
2024-04-04 16:53:48 +0200

    milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,21 +7,45 @@ export default {
             state
         }
     },
-    mounted() {
-        state.callApi()
+    methods: {
+        searchMovies() {
+            const url = `${state.base_movies_api_url}?api_key=${state.key_api}&query=${state.searchFilm}`;
+            //console.log(url);
+            this.state.callApi(url)
+        }
     }
+
 }
 </script>
 
-<template>
-    <h1>Boolflix</h1>
+<template >
+    <div>
+        <h1>Boolflix</h1>
 
-    <ul>
-        <li v-for="result in state.results">
-            {{ result.original_title }}
-        </li>
-    </ul>
+        <input type="text" v-model="state.searchFilm" @keyup.enter="searchMovies()">
+        <button @click="searchMovies()">CERCA</button>
+
+        <ul>
+            <li v-for="result in state.results">
+                <div>{{ result.title }}</div>
+                <div>{{ result.original_title }}</div>
+                <div>{{ result.original_language }}</div>
+                <div>{{ result.vote_average }}</div>
+            </li>
+        </ul>
+    </div>
 
 </template>
 
-<style></style>
+<style>
+
+li{
+    border: 1px solid black;
+    margin: 3px;
+    list-style: none;
+    border-radius: 8px;
+    color: white;
+    background-color: rgb(27, 27, 27);
+}
+
+</style>
--- src/state.js
+++ src/state.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 
 export const state = reactive({
     results: [],
-    base_movies_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=fbdbb4bc9501aa5963bc9186e57dcf80&query=harry',
+    base_movies_api_url: 'https://api.themoviedb.org/3/search/movie',
     key_api: 'fbdbb4bc9501aa5963bc9186e57dcf80',
     searchFilm: '',
     
 
-    callApi() {
-        axios.get(this.base_movies_api_url).then(resp => {
+    callApi(url) {
+        axios.get(url).then(resp => {
             console.log(resp);
             this.results = resp.data.results
         })
