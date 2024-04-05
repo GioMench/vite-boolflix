da96cd7a4668722f29697b555f7d07fb6c206616
GioMench <giorgiaglm@gmail.com>
2024-04-05 13:52:19 +0200

    add serie tv

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -11,18 +11,25 @@ export default {
         searchMovies() {
             const url = `${state.base_movies_api_url}?api_key=${state.key_api}&query=${state.searchFilm}`;
             //console.log(url);
-            //const urlSerie = `${state.base_series_api_url}?api_key=${state.key_api}&query=${state.searchFilm}`;
-            this.state.callApi(url);
-         
+            const urlSerie = `${state.base_series_api_url}?api_key=${state.key_api}&query=${state.searchFilm}`;
+            this.state.callApi(url, urlSerie);
+            
+            /*this.state.callApiTv();*/
+            
+
         },
 
+        //searchTv(){}
+           
         
+
+
     }
 
 }
 </script>
 
-<template >
+<template>
     <div>
         <h1>Boolflix</h1>
 
@@ -32,13 +39,34 @@ export default {
         <ul>
             <li class="card" v-for="result in state.results">
                 <div>
-                    <img class="cover" :src="'http://image.tmdb.org/t/p/w185/' + result.poster_path " alt="">
+                    <img class="cover" :src="'http://image.tmdb.org/t/p/w185/' + result.poster_path" alt="">
                 </div>
                 <div>{{ result.title }}</div>
                 <div>{{ result.original_title }}</div>
-                <div> <img class="flag" :src="'/public/1x1/' + result.original_language + '.svg'" alt=""></div>
+
+                <div > 
+                    <img class="flag" :src="'/public/1x1/' + result.original_language + '.svg'" alt="">
+                </div>
                 <div>{{ result.original_language }}</div>
-                <div> {{Math.trunc(result.vote_average)}}</div>
+                
+
+                <div> {{ Math.trunc(result.vote_average) }}</div>
+            </li>
+
+            <li class="card" v-for="result in state.resultsTv">
+                <div>
+                    <img class="cover" :src="'http://image.tmdb.org/t/p/w185/' + result.poster_path" alt="">
+                </div>
+                <div>{{ result.title }}</div>
+                <div>{{ result.original_title }}</div>
+
+                <div > 
+                    <img class="flag" :src="'/public/1x1/' + result.original_language + '.svg'" alt="">
+                </div>
+                <div>{{ result.original_language }}</div>
+                
+
+                <div> {{ Math.trunc(result.vote_average) }}</div>
             </li>
         </ul>
     </div>
@@ -46,24 +74,21 @@ export default {
 </template>
 
 <style>
-
-.card{
+.card {
     border: 1px solid black;
     margin: 3px;
     list-style: none;
     border-radius: 8px;
     color: white;
     background-color: rgb(27, 27, 27);
-    width: 250px;  
-    padding: 8px;  
+    width: 250px;
+    padding: 8px;
     height: 420px;
 }
-.cover{
-    margin-bottom: 10px;
-}
 
-.flag{
+
+.flag {
     width: 20px;
+    height: 13px;
 }
-
 </style>
--- src/state.js
+++ src/state.js
@@ -3,16 +3,24 @@ import axios from 'axios';
 
 export const state = reactive({
     results: [],
+    resultsTv:[],
     base_movies_api_url: 'https://api.themoviedb.org/3/search/movie',
     key_api: '020af5f4a170366144c3befd4f42b017',
     searchFilm: '',
     base_series_api_url: 'https://api.themoviedb.org/3/search/tv',
     
 
-    callApi(url) {
+    callApi(url,urlSerie) {
         axios.get(url).then(resp => {
             console.log(resp);
             this.results = resp.data.results
+        }),
+        axios.get(urlSerie).then(resp => {
+            console.log(resp);
+            this.resultsTv = resp.data.results
         })
-    }
-})
+
+    },
+
+
+})  
